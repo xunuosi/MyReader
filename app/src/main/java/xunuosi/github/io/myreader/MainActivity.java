759e6ee23ec0a4b93fa8bd94fb2eabd4bfdac1fa
tf_xunuosi <xunuosi19871202@gmail.com>
2017-04-06 15:58:04 +0800

    正确合入FBReaderJ项目

--- app/src/main/java/xunuosi/github/io/myreader/MainActivity.java
+++ app/src/main/java/xunuosi/github/io/myreader/MainActivity.java
@@ -1,7 +1,11 @@
 package xunuosi.github.io.myreader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import org.geometerplus.android.fbreader.FBReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openBook(View view) {
+        Intent intent = new Intent(MainActivity.this, FBReader.class);
+        startActivity(intent);
     }
 }
